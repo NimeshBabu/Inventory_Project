46c6dae7eed69e9f29d638eff2e65f72748e56c6
Pratibha Sharma <pratibhaa625@gmail.com>
2025-03-28 11:39:42 +0545

    completed project

--- inventrix.sql
+++ inventrix.sql
@@ -1,32 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 27, 2025 at 09:25 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+CREATE DATABASE inventrix;
+USE inventrix;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `inventrix`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product`
-CREATE DATABASE inventrix;
-USE inventrix;
 
 CREATE TABLE `product` (
   `ProductCode` varchar(10) NOT NULL,
@@ -39,16 +19,13 @@ CREATE TABLE `product` (
   `Supplier-PAN` varchar(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `product`
---
 
 INSERT INTO `product` (`ProductCode`, `ProductName`, `Cost`, `Price`, `Description`, `Url`, `Quantity`, `Supplier-PAN`) VALUES
 ('1221', 'Dining Table', 30000.00, 37000.00, '4 chair\r\n', 'https://i5.walmartimages.com/seo/SEGMART-Dining-Table-4-High-back-Upholstered-Chairs-Modern-Dinette-Set-Chairs-Set-Persons-Small-Home-Kitchen-Ideal-Apartment-Breakfast-Nook_bc8ea994-0caa-44fc-9fd9-c287225f7408.aa04dbb32beb866b7e87757d95a5c299.jpeg', 0, 'ABC Company-1111'),
-('1254', 'Bed', 50000.00, 60000.00, 'King Size Bed', 'https://lifely.com.au/cdn/shop/files/JoseBedFrameGallery1.jpg?v=1733883102', 1, 'ABC Company-1111'),
-('1578', 'Study Table', 18000.00, 25000.00, 'Aesthetic', 'https://nativesutra.com/wp-content/uploads/2024/12/Screenshot_3-1.png', 5, 'ABC Company-1111'),
+('1254', 'Bed', 50000.00, 60000.00, 'King Size Bed', 'https://lifely.com.au/cdn/shop/files/JoseBedFrameGallery1.jpg?v=1733883102', 25, 'ABC Company-1111'),
+('1578', 'Study Table', 18000.00, 25000.00, 'Aesthetic', 'https://nativesutra.com/wp-content/uploads/2024/12/Screenshot_3-1.png', 25, 'ABC Company-1111'),
 ('1982', 'Sofa', 30000.00, 35000.00, '3 piece,brown', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0sgzWXCBhGxlllcZ-DeQf3LPlUnzka5Wr1Q&s', 0, 'ABC Company-1111'),
-('5214', 'Kitchen Set', 100000.00, 1400000.00, 'Full set', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwPLSjGDXg-yiQb2AurSMv_-fWd2zvSGFS-A&s', 27, 'Sunshine Traders-1357'),
+('5214', 'Kitchen Set', 100000.00, 1400000.00, 'Full set', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwPLSjGDXg-yiQb2AurSMv_-fWd2zvSGFS-A&s', 24, 'Sunshine Traders-1357'),
 ('7412', 'Wardrobe', 50000.00, 60000.00, 'Grey Color ', 'https://graphql.smithandfabricators.com/media/products/10_Grey_Wardrobe_Designs_Ideas_Modern_Interior_Look_722e8e20.jpeg', 5, 'ABC Company-1111');
 
 -- --------------------------------------------------------
@@ -67,18 +44,16 @@ CREATE TABLE `purchase` (
   `Supplier-PAN` varchar(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `purchase`
---
 
 INSERT INTO `purchase` (`PurchaseID`, `Date`, `ProductCode`, `Quantity`, `PurchaseAmount`, `PaymentStatus`, `Supplier-PAN`) VALUES
 (1229, '2025-03-05', '1221', 25, 750000.00, 'Paid', 'ABC Company-1111'),
-(1231, '2025-03-11', '1221', 10, 300000.00, 'Paid', 'ABC Company-1111'),
 (1232, '2025-03-20', '1221', 20, 600000.00, 'Paid', 'ABC Company-1111'),
-(1233, '2025-03-18', '1221', 5, 150000.00, 'Paid', 'ABC Company-1111'),
+(1233, '2025-03-18', '1221', 5, 150000.00, 'Unpaid', 'ABC Company-1111'),
 (1234, '2025-03-17', '1221', 5, 150000.00, 'Paid', 'ABC Company-1111'),
 (1235, '2025-03-12', '1221', 30, 900000.00, 'Paid', 'ABC Company-1111'),
-(1237, '2025-03-19', '5214', 25, 2500000.00, 'Paid', 'Sunshine Traders-1357');
+(1237, '2025-03-19', '5214', 25, 2500000.00, 'Paid', 'Sunshine Traders-1357'),
+(1238, '2025-02-26', '1578', 30, 540000.00, 'Due', 'ABC Company-1111'),
+(1239, '2025-03-06', '1254', 25, 1250000.00, 'Paid', 'ABC Company-1111');
 
 -- --------------------------------------------------------
 
@@ -99,15 +74,19 @@ CREATE TABLE `sales` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `sales`
 --
 
 INSERT INTO `sales` (`SaleID`, `Date`, `ProductCode`, `Customer`, `ShippingAddress`, `Biller`, `Quantity`, `SalesAmount`, `PaymentStatus`) VALUES
-(4, '2025-03-04', '1221', 'Ghanshyam ', 'Palpa', 'Biller A', 30, 1110000.00, 'Paid'),
-(11, '2025-03-12', '1221', 'hari', 'Bhaktapur', 'Biller A', 5, 185000.00, 'Paid'),
+(4, '2025-03-04', '1221', 'Ghanshyam ', 'Palpa', 'Biller A', 30, 1110000.00, 'Unpaid'),
 (12, '2025-03-06', '1221', 'Ram ', 'Bhaktapur', 'Biller A', 10, 370000.00, 'Paid'),
-(13, '2025-03-04', '1578', 'Ghanshyam ', 'Bhaktapur', 'Biller A', 5, 125000.00, 'Paid'),
-(14, '2025-03-13', '1982', 'Ram ', 'Palpa', 'Biller B', 3, 105000.00, 'Paid');
+(13, '2025-03-04', '1578', 'Ghanshyam ', 'Bhaktapur', 'Biller A', 5, 125000.00, 'Due'),
+(15, '2025-03-07', '1982', 'Nimesh', 'Palpa', 'Biller C', 10, 350000.00, 'Unpaid'),
+(16, '2025-03-07', '1982', 'Nimesh', 'Palpa', 'Biller C', 10, 350000.00, 'Unpaid'),
+(17, '2025-03-07', '1982', 'Nimesh', 'Palpa', 'Biller C', 10, 350000.00, 'Unpaid'),
+(18, '2025-03-07', '1982', 'Nimesh', 'Palpa', 'Biller C', 10, 350000.00, 'Unpaid'),
+(19, '2025-03-18', '1578', 'Nimesh', 'Bhaktapur', 'Biller B', 10, 250000.00, 'Due'),
+(20, '2025-03-05', '1254', 'Nimesh', 'Bhaktapur', 'Biller B', 1, 60000.00, 'Due'),
+(21, '2025-03-04', '5214', 'Nimesh', 'Palpa', 'Biller B', 3, 4200000.00, 'Unpaid');
 
 -- --------------------------------------------------------
 
@@ -125,11 +104,11 @@ CREATE TABLE `supplier` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `supplier`
 --
 
 INSERT INTO `supplier` (`Supplier-PAN`, `Supplier`, `Email`, `PhoneNo`, `Address`, `PANNo`) VALUES
 ('ABC Company-1111', 'ABC Company', 'abc@email.com', '9843350001', 'Bagbazar', '1111'),
+('HelloWorld Traders-2586', 'HelloWorld Traders', 'hello_world@gmail.com', '9843350001', 'Bagbazar', '2586'),
 ('Sunshine Traders-1357', 'Sunshine Traders', 'sunshine@gmail.com', '9843695481', 'Greenland', '1357');
 
 -- --------------------------------------------------------
@@ -142,16 +121,16 @@ CREATE TABLE `user` (
   `s_no` int(11) NOT NULL,
   `email` varchar(25) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `date` datetime NOT NULL DEFAULT current_timestamp()
+  `date` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`s_no`, `email`, `password`, `date`) VALUES
-(0, 'kritisha@1', '$2y$10$TJDtN2XtuyAudrH9Pfp5pOZr3dm82RG/teigRrDOY6bx5I/jNRTdG', '2025-03-24 14:15:48'),
-(1, 'kritisha@123', '$2y$10$yLspMkzxEKjLeVQ4E9pwvOIFl0a/Q5DTWQDs7xnJeVX.nc8yA1pJq', '2025-03-23 15:09:22');
+(3, 'Kritisha123@gmail.com', '$2y$10$nF.mlRniMuEgLgTcwNya8uz6quU27RBVTDkuH8ZhyX8fAjUkFi/ZO', '2025-03-27'),
+(4, 'Nimesh123@gmail.com', '$2y$10$6MMxsoQdoH7bSBhbf./yx.LsYcufnCXZINnrkarmtNRwvU1Qgmt1.', '2025-03-27'),
+(5, 'Pratibha123@gmail.com', '$2y$10$0iyI0pPmwPz4Wv8cgbDRl.yW/cu.LaLWpzg.nSsWf.C0Gz2IVzcNq', '2025-03-27');
 
 --
 -- Indexes for dumped tables
@@ -201,13 +180,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `purchase`
 --
 ALTER TABLE `purchase`
-  MODIFY `PurchaseID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1238;
+  MODIFY `PurchaseID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1240;
 
 --
 -- AUTO_INCREMENT for table `sales`
 --
 ALTER TABLE `sales`
-  MODIFY `SaleID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `SaleID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `s_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -231,7 +216,3 @@ ALTER TABLE `purchase`
 ALTER TABLE `sales`
   ADD CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`ProductCode`) REFERENCES `product` (`ProductCode`) ON UPDATE CASCADE;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
