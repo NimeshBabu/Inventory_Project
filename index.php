1b0fbf8bb544d8aa561516ea351a0e8df4670c3c
Nimesh Babu Thapa <nikeshthapa2005@gmail.com>
2025-04-01 17:52:48 +0545

    Small change

--- index.php
+++ index.php
@@ -418,7 +418,7 @@
 
         <!-- Footer-->
         <footer class="bg-light py-5">
-            <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; 2023 INVENTRIX | All Rights Reserved</div></div>
+            <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; 2025 INVENTRIX | All Rights Reserved</div></div>
         </footer>
 
 
